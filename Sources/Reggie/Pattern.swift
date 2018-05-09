e341ad5cb7f64f05ce8f0b3638189999a9b3c0bf
phrz <phrz@me.com>
2018-05-09 00:13:34 -0500

    .gitignore

---
+++ Sources/Reggie/Pattern.swift
@@ -0,0 +1,9 @@
+//
+//  Pattern.swift
+//  Reggie
+//
+//  Created by Paul Herz on 2018-05-03.
+//  Copyright © 2018 Paul Herz. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Reggie/Validators.swift
@@ -0,0 +1,9 @@
+//
+//  Validators.swift
+//  Reggie
+//
+//  Created by Paul Herz on 2018-05-08.
+//  Copyright © 2018 Paul Herz. All rights reserved.
+//
+
+import Foundation
